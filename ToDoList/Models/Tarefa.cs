abdfbd196438f33440ca5cace29440fea0280b2b
Diogo Grings Graebin <diogograebin@gmail.com>
2025-08-11 22:20:37 -0300

    Adiciona Model de Tarefa

---
+++ ToDoList/Models/Tarefa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class Tarefa
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O título é obrigatório.")]  // id unico da tarefa
+        [StringLength(100, ErrorMessage = "O título não pode ter mais de 100 caracteres.")] //mensagem de erro personalizada
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+
+        public bool Concluida { get; set; }
+
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+
+        public DateTime? DataConclusao { get; set; }
+
+
+    }
+}
