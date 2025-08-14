9dbde483527d43e06362b8c230c0a3b78612d9cc
Diogo Grings Graebin <diogograebin@gmail.com>
2025-08-13 22:33:57 -0300

    Primeira versão do TarefasController

---
+++ ToDoList/Controllers/TarefasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Data;
+using ToDoList.Models;
+using Microsoft.EntityFrameworkCore; 
+
+namespace ToDoList.Controllers
+{
+    public class TarefasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TarefasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Lista e formulário de criação
+
+        public async Task<IActionResult> Index()
+        {
+            var tarefas = await _context.Tarefas.OrderByDescending(t => t.DataCriacao).ToListAsync();
+            return View(tarefas);
+        }
+    }
+}
